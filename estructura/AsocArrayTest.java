c62813993dbd895130645e578e0292ae82773326
Isabel RM <isabelroche@uma.es>
2017-05-23 21:07:47 +0200

    red: una tabla recién creada está vacía

--- estructura/AsocArrayTest.java
+++ estructura/AsocArrayTest.java
@@ -7,8 +7,9 @@ import org.junit.Test;
 public class AsocArrayTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void tamañoDeTablaVaciaEs0() {
+		AsocArray miTabla = new AsocArray();
+		assertEquals(miTabla.size(),1);
 	}
 
 }
