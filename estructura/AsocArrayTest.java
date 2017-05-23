10d7ad1ef16076b486ebddd6f576f9ee1e992c3d
Isabel RM <isabelroche@uma.es>
2017-05-23 21:17:39 +0200

    refactor: Añado @before que inicializa la tabla

--- estructura/AsocArrayTest.java
+++ estructura/AsocArrayTest.java
@@ -2,21 +2,29 @@ package estructura;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class AsocArrayTest {
+	
+	AsocArray miTabla;
+	
+	@Before
+	public void setUp(){
+		miTabla = new AsocArray();
+	}
 
 	@Test
 	public void tamañoDeTablaVaciaEs0() {
-		AsocArray miTabla = new AsocArray();
 		assertEquals(miTabla.size(),0);
 	}
 	
 	@Test
 	public void tamañoDeTablaConUnPutEs1() {
-		AsocArray miTabla = new AsocArray();
 		miTabla.put("nombre", "luis");
 		assertEquals(miTabla.size(),1);
 	}
+	
+	
 
 }
